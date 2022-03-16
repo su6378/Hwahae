af8a083fe2f99efd474a6f906e93424f065c47e8
Administrator <josooyeon11@naver.com>
2022-03-16 18:34:48 +0900

    NowFragment페이지 광고 뷰페이저 추가

--- app/src/main/java/com/birdview/hwahae/main/home/AdVP.kt
+++ app/src/main/java/com/birdview/hwahae/main/home/AdVP.kt
@@ -1,6 +1,7 @@
-package com.birdview.hwahae.login
+package com.birdview.hwahae.main.home
 
 import android.content.Context
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ImageView
@@ -8,23 +9,21 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.birdview.hwahae.R
 
-class LoginVP (private val context: Context, loginList: ArrayList<Int>) : RecyclerView.Adapter<LoginVP.PagerViewHolder>() {
-    var item = loginList
+class AdVP (private val context: Context, adList: MutableList<Uri>) : RecyclerView.Adapter<AdVP.PagerViewHolder>() {
+    var item = adList
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = PagerViewHolder((parent))
 
     override fun getItemCount(): Int = item.size
 
     override fun onBindViewHolder(holder: PagerViewHolder, position: Int) {
-        //이미지 소스 적용
-        //holder.image.setImageResource(item[position])
         //글라이드 패키지 적용
-        Glide.with(context).load(item[position]).into(holder.image)
+        Glide.with(context).load(item[position]).fitCenter().into(holder.ad_image)
     }
 
     inner class PagerViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder
-        (LayoutInflater.from(parent.context).inflate(R.layout.login_image_item, parent, false)){
+        (LayoutInflater.from(parent.context).inflate(R.layout.main_home_now_ad_item, parent, false)){
 
-        val image = itemView.findViewById<ImageView>(R.id.login_image)
+        val ad_image = itemView.findViewById<ImageView>(R.id.ad_image)
     }
 }
--- app/src/main/java/com/birdview/hwahae/main/home/NowFragment.kt
+++ app/src/main/java/com/birdview/hwahae/main/home/NowFragment.kt
@@ -1,5 +1,6 @@
 package com.birdview.hwahae.main.home
 
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -7,37 +8,57 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
 import com.birdview.hwahae.R
+import com.birdview.hwahae.databinding.LoginFindPageBinding
+import com.birdview.hwahae.databinding.MainHomeNowPageBinding
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
+import com.tbuonomo.viewpagerdotsindicator.WormDotsIndicator
 
 
 class NowFragment : Fragment() {
 
+    //뷰바인딩
+    private var mBinding: MainHomeNowPageBinding? = null
+    private val binding get() = mBinding!!
+
     //이 제품 어때요?
-    private lateinit var recommend_recyclerview : RecyclerView
+    private lateinit var recommend_recyclerview: RecyclerView
     private lateinit var recommendAdapter: RecommendAdapter
     private val recommendData = mutableListOf<RecommendData>()
 
     //NEW인기신제품
-    private lateinit var newnhot_recyclerview : RecyclerView
+    private lateinit var newnhot_recyclerview: RecyclerView
     private lateinit var newnhotAdapter: NewnHotAdapter
     private val newnhotData = mutableListOf<NewnHotData>()
+
+    //광고 뷰페이저
+    private lateinit var ad_vp : ViewPager2
+    private lateinit var adVP : AdVP
+    private val adData = mutableListOf<Uri>()
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view: View = inflater.inflate(R.layout.main_home_now_page, container, false)
+
+        mBinding = MainHomeNowPageBinding.inflate(inflater,container,false)
 
         //이 제품 어때요?
-        recommend_recyclerview = view.findViewById(R.id.recommend_recyclerview)
+        recommend_recyclerview = binding.recommendRecyclerview
         initRecommend()
 
         //NEW인기신제품
-        newnhot_recyclerview = view.findViewById(R.id.newnhot_recyclerview)
+        newnhot_recyclerview = binding.newnhotRecyclerview
         initNewnHot()
-        return view
+
+        //광고 뷰페이저
+        initAd()
+
+        return binding.root
     }
 
     //이 제품 어때요? 리사이클러뷰 초기화
@@ -45,16 +66,22 @@ class NowFragment : Fragment() {
         recommendAdapter = RecommendAdapter(requireContext())
         recommend_recyclerview.adapter = recommendAdapter
 
-        val productList = arrayListOf<String>("simple_toner","teatree_toner","greentea_toner")
-        val companyList = arrayListOf<String>("싸이닉","메디힐","시드물")
-        val nameList = arrayListOf<String>("더 심플 카밍 토너","티트리바이옴 블레미쉬\n시카 토너","녹차 스킨")
+        val productList = arrayListOf<String>("simple_toner", "teatree_toner", "greentea_toner")
+        val companyList = arrayListOf<String>("싸이닉", "메디힐", "시드물")
+        val nameList = arrayListOf<String>("더 심플 카밍 토너", "티트리바이옴 블레미쉬\n시카 토너", "녹차 스킨")
 
-        for(i in 0..2){
-            Firebase.storage.reference.child("product/"+productList[i]+".png").downloadUrl.addOnCompleteListener {
+        for (i in 0..2) {
+            Firebase.storage.reference.child("product/" + productList[i] + ".png").downloadUrl.addOnCompleteListener {
                 if (it.isSuccessful) {
 
                     recommendData.apply {
-                        add(RecommendData(image = it.result, company = companyList[i], name = nameList[i]))
+                        add(
+                            RecommendData(
+                                image = it.result,
+                                company = companyList[i],
+                                name = nameList[i]
+                            )
+                        )
 
                         recommendAdapter.datas = recommendData
                         recommendAdapter.notifyDataSetChanged()
@@ -64,16 +91,16 @@ class NowFragment : Fragment() {
             }
         }
 
-        recommendAdapter.setOnItemClickListener(object : RecommendAdapter.OnItemClickListener{
-            override fun onItemClick(v: View, data: RecommendData, pos : Int) {
-                Log.d("테스트",data.name)
-            /*  Intent(this@MainActivity, ProfileDetailActivity::class.java).apply {
-                    putExtra("data", data)
-                    addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                }.run { startActivity(this) }*/
+        recommendAdapter.setOnItemClickListener(object : RecommendAdapter.OnItemClickListener {
+            override fun onItemClick(v: View, data: RecommendData, pos: Int) {
+                /*  Intent(this@MainActivity, ProfileDetailActivity::class.java).apply {
+                        putExtra("data", data)
+                        addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    }.run { startActivity(this) }*/
             }
 
         })
+
 
     }
 
@@ -82,16 +109,36 @@ class NowFragment : Fragment() {
         newnhotAdapter = NewnHotAdapter(requireContext())
         newnhot_recyclerview.adapter = newnhotAdapter
 
-        val productList = arrayListOf<String>("retinol_ample","dokdo_suncream","birch_sunstick","aqua_eyecream","divein_suncream","chamomile_lipbalm")
-        val companyList = arrayListOf<String>("이니스프리","라운드랩","라운드랩","에스네이처","토리든","비플레인")
-        val nameList = arrayListOf<String>("레티놀 시카 흔적 앰플","1025 독도 선크림\n[SPF50+/PA++++]","자작나무 수분 선스틱\n[SPF50+/PA++++]","아쿠아 소이 요거\n아이크림","다이브인 무기자차\n마일드 선크림...","캐모마일 듀얼 보습\n립밤")
+        val productList = arrayListOf<String>(
+            "retinol_ample",
+            "dokdo_suncream",
+            "birch_sunstick",
+            "aqua_eyecream",
+            "divein_suncream",
+            "chamomile_lipbalm"
+        )
+        val companyList = arrayListOf<String>("이니스프리", "라운드랩", "라운드랩", "에스네이처", "토리든", "비플레인")
+        val nameList = arrayListOf<String>(
+            "레티놀 시카 흔적 앰플",
+            "1025 독도 선크림\n[SPF50+/PA++++]",
+            "자작나무 수분 선스틱\n[SPF50+/PA++++]",
+            "아쿠아 소이 요거\n아이크림",
+            "다이브인 무기자차\n마일드 선크림...",
+            "캐모마일 듀얼 보습\n립밤"
+        )
 
-        for(i in 0..5){
-            Firebase.storage.reference.child("product/"+productList[i]+".png").downloadUrl.addOnCompleteListener {
+        for (i in 0..5) {
+            Firebase.storage.reference.child("product/" + productList[i] + ".png").downloadUrl.addOnCompleteListener {
                 if (it.isSuccessful) {
 
                     newnhotData.apply {
-                        add(NewnHotData(image = it.result, company = companyList[i], name = nameList[i]))
+                        add(
+                            NewnHotData(
+                                image = it.result,
+                                company = companyList[i],
+                                name = nameList[i]
+                            )
+                        )
 
                         newnhotAdapter.datas = newnhotData
                         newnhotAdapter.notifyDataSetChanged()
@@ -102,14 +149,52 @@ class NowFragment : Fragment() {
             }
         }
 
-
-
-        newnhotAdapter.setOnItemClickListener(object : NewnHotAdapter.OnItemClickListener{
-            override fun onItemClick(v: View, data: NewnHotData, pos : Int) {
-                Log.d("테스트",data.name)
+        newnhotAdapter.setOnItemClickListener(object : NewnHotAdapter.OnItemClickListener {
+            override fun onItemClick(v: View, data: NewnHotData, pos: Int) {
+                Log.d("테스트", data.name)
             }
 
         })
 
     }
+
+    //광고 뷰페이저
+    private fun initAd() {
+
+        ad_vp = binding.adVp
+        adVP = AdVP(requireContext(),getAdList())
+        ad_vp.adapter = adVP
+        ad_vp.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+
+        //indicator 부분 연결
+        binding.adIndicator.setViewPager2(ad_vp)
+
+    }
+
+    //뷰페이저 이미지
+    private fun getAdList(): MutableList<Uri> {
+        val adList = arrayListOf<String>("lifill", "celderma")
+
+        for (i in 0..1) {
+            Firebase.storage.reference.child("ad/" + adList[i] + ".png").downloadUrl.addOnCompleteListener {
+                if (it.isSuccessful) {
+
+
+                    adData.apply {
+                        add(
+                           it.result
+                        )
+
+                        adVP.item = adData
+                        adVP.notifyDataSetChanged()
+                        Log.d("xptmxm",adData.size.toString())
+
+                    }
+                }
+
+            }
+        }
+        return adData
+    }
+
 }
